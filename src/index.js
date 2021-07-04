5607db21fdc0fc0744479132794f2606f0d8e70b
Leeharim__kr <hw05165@naver.com>
2021-07-04 16:39:21 +0900

    #1.1-2 useInput bulid custom hook useing 'useState'

--- src/index.js
+++ src/index.js
@@ -3,51 +3,38 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initalValue, validators) => {
+  const [value, setValue] = useState(initalValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = [];
+    validators.forEach((v, i) => (willUpdate[i] = true));
+    validators.forEach((v, i) => {
+      if (typeof validators[i] === "function") {
+        willUpdate[i] = validators[i](value);
+      }
+    });
+    if (willUpdate.every((v) => v === true)) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  const invalid = (value) => !value.includes("@");
+  const isTrue = true;
+  const name = useInput("Mr.", [maxLen, invalid, isTrue]);
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
 
-class AppUgly extends React.Component {
-  state = {
-    item: 1
-  };
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="AppUgly">
-        <h1>Hello {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
-  }
-  incrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () => {
-    this.setState((state) => {
-      return {
-        item: state.item - 1
-      };
-    });
-  };
-}
-
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-// ReactDOM.render(<AppUgly />, rootElement);
