89ccb0635a3b39cbc423a835b8e06134c10fbaca
Leeharim__kr <hw05165@naver.com>
2021-07-05 13:43:32 +0900

    #1.3 useTabs build useTabs with useState hook

--- src/index.js
+++ src/index.js
@@ -3,8 +3,38 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const content = [
+  {
+    tab: "section 1",
+    content: "I'm the content of section 1"
+  },
+  {
+    tab: "section 2",
+    content: "I'm the content of section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
 const App = () => {
-  return <div className="App"></div>;
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
