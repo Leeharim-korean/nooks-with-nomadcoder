c4c2fdeef360f4bea12b6543fc340e5cd50a1d3c
Leeharim__kr <hw05165@naver.com>
2021-07-05 15:56:18 +0900

    #2.2 useClick build useClick with useEffect, useRef hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useClick = (onClick) => {
+  const element = useRef();
+  if (typeof onClick !== "function") {
+    return;
+  }
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
 const App = () => {
-  return <div className="App"></div>;
+  const sayHello = () => console.log("say Hello");
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
