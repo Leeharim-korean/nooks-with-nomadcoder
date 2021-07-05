369b0af1b6d9f5d8b2e0b3eb4b4d274d3cb66346
Leeharim__kr <hw05165@naver.com>
2021-07-05 14:15:53 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  // useEffect(sayHello, [number]); //has error
-  const [number, setNumber] = useState(0);
-  const [aNumber, setANumber] = useState(0);
-  // useEffect(sayHello, [number]);
-  useEffect(sayHello, []);
-  return (
-    <div className="App">
-      <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
