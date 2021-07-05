4ca111ad52f5d7c206ce7142e0e499400ad8a353
Leeharim__kr <hw05165@naver.com>
2021-07-05 14:10:01 +0900

    #2.0 Introduction to useEffect useEffect(effect, deps) work like componentDidMount(effect) and componentDidUpdate(deps) when deps change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const sayHello = () => console.log("Hello");
+  // useEffect(sayHello, [number]); //has error
+  const [number, setNumber] = useState(0);
+  const [aNumber, setANumber] = useState(0);
+  // useEffect(sayHello, [number]);
+  useEffect(sayHello, []);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
