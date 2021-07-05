ed6a916cbcf29355869d1c174ad582f18cf48b4f
Leeharim__kr <hw05165@naver.com>
2021-07-05 14:30:28 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,24 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
-
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 5000);
-  return (
-    <div className="App">
-      <div>Hi</div>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
