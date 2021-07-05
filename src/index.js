c3f34bef55f5a87b154a7a49ac3f418f7fc3d4cb
Leeharim__kr <hw05165@naver.com>
2021-07-05 13:21:12 +0900

    Delete previous content start new lecture

--- src/index.js
+++ src/index.js
@@ -3,37 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initalValue, validators) => {
-  const [value, setValue] = useState(initalValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = [];
-    validators.forEach((v, i) => (willUpdate[i] = true));
-    validators.forEach((v, i) => {
-      if (typeof validators[i] === "function") {
-        willUpdate[i] = validators[i](value);
-      }
-    });
-    if (willUpdate.every((v) => v === true)) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
-
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  const invalid = (value) => !value.includes("@");
-  const isTrue = true;
-  const name = useInput("Mr.", [maxLen, invalid, isTrue]);
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
