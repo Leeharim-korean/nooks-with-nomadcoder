0e113cd8b210d23b45797b14961632f1d9c1f444
Leeharim__kr <hw05165@naver.com>
2021-07-06 15:11:54 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useHover = (onHover) => {
-  const element = useRef();
-  if (typeof onHover !== "function") {
-    return;
-  }
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("mouseenter", onHover);
-    }
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("mouseenter", onHover);
-      }
-    };
-  }, []);
-  return element;
-};
-
 const App = () => {
-  const sayHello = () => console.log("say Hello");
-  const title = useHover(sayHello);
-  return (
-    <div className="App">
-      <h1 ref={title}>Hi</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
