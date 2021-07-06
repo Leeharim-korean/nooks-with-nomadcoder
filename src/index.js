21a75e82d507a484f299a85eee978644c532e556
Leeharim__kr <hw05165@naver.com>
2021-07-06 15:45:03 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
-// import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useConfirm = (message = "", onConfirm, onCancel) => {
-  if (typeof onConfirm !== "function") {
-    return;
-  }
-  if (onCancel && typeof onCancel !== "function") {
-    return;
-  }
-  const confirmAction = () => {
-    if (window.confirm(message)) {
-      onConfirm();
-    } else {
-      onCancel();
-    }
-  };
-  return confirmAction;
-};
-
 const App = () => {
-  const deleteWorld = () => console.log("Deleting the world...");
-  const abort = () => console.log("Aborted");
-  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
-  return (
-    <div className="App">
-      <button onClick={confirmDelete}>Delete World</button>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
