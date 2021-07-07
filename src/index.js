92de22f1629a402ea06b19108286db3ec40c10dd
Leeharim__kr <hw05165@naver.com>
2021-07-07 15:39:08 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,35 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-export const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine || true);
-  const handleChange = () => {
-    if (onChange && typeof onChange === "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
-  };
-  useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-    () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
-  }, []);
-  return status;
-};
-
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline");
-  };
-  const onLine = useNetwork(handleNetworkChange);
-  return (
-    <div className="App">
-      <h1>{onLine ? "Online" : "Offline"}</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
