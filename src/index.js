ff1bc2142346bb0ecbc2191cf53d5ff99133258e
Leeharim__kr <hw05165@naver.com>
2021-07-07 15:57:51 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,56 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useFullscreen = (callback) => {
-  const element = useRef();
-  const runCb = (isFull) => {
-    if (callback && typeof callback === "function") {
-      callback(isFull);
-    }
-  };
-  const triggerFull = () => {
-    if (element.current) {
-      if (element.current.requestFullscreen) {
-        element.current.requestFullscreen();
-      } else if (element.current.mozRequestFullScreen) {
-        element.current.mozRequestFullScreen();
-      } else if (element.current.webkitRequestFullscreen) {
-        element.current.webkitRequestFullscreen();
-      } else if (element.current.msRequestFullscreen) {
-        element.current.msRequestFullscreen();
-      }
-      runCb(true);
-    }
-  };
-  const exitFull = () => {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-    runCb(false);
-  };
-  return { element, triggerFull, exitFull };
-};
-
 const App = () => {
-  const onFullS = (isFull) => {
-    console.log(isFull ? "We are full" : "We are small");
-  };
-  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
-  return (
-    <div className="App" style={{ height: "1000vh" }}>
-      <div ref={element}>
-        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" width="250" />
-        <button onClick={exitFull}>Exit Fullscreen</button>
-        <button onClick={triggerFull}>Make Fullscreen</button>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
