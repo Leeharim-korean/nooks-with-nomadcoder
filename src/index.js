302000f81df78b445f78713c7dea989daf778c18
Leeharim__kr <hw05165@naver.com>
2021-07-07 13:59:30 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useFadeIn = (duration = 1, delay = 0) => {
-  const element = useRef();
-  if (typeof duration !== "number" || typeof delay !== "number") {
-    return;
-  }
-  useEffect(() => {
-    if (element.current) {
-      const { current } = element;
-      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
-      current.style.opacity = 1;
-    }
-  }, []);
-  return { ref: element, style: { opacity: 0 } };
-};
-
 const App = () => {
-  const fadeInH1 = useFadeIn(1, 2);
-  const fadeInP = useFadeIn(5, 3);
   return (
     <div className="App">
-      <h1 {...fadeInH1}>Hi</h1>
-      <p {...fadeInP}>lalalalala I have control</p>
+      <h1>Hi</h1>
     </div>
   );
 };
