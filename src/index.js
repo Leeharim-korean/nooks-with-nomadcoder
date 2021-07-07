05478ec7848d0be93599bc85a1139e0560c5116d
Leeharim__kr <hw05165@naver.com>
2021-07-07 15:44:09 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,28 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useScroll = () => {
-  const [state, setState] = useState({
-    x: 0,
-    y: 0
-  });
-  const onScroll = () => {
-    setState({ y: window.scrollY, x: window.scrollX });
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-  return state;
-};
-
 const App = () => {
-  const { y } = useScroll();
-  return (
-    <div className="App" style={{ height: "1000vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
-    </div>
-  );
+  return <div className="App" style={{ height: "1000vh" }}></div>;
 };
 
 const rootElement = document.getElementById("root");
