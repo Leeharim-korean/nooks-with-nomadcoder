828b284c3c4f9e4e73ab0615afbad2329cab7597
Leeharim__kr <hw05165@naver.com>
2021-07-07 12:25:22 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,25 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useBeforeLeave = (onBefore) => {
-  if (typeof onBefore !== "function") {
-    return;
-  }
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
-  }, []);
-};
-
 const App = () => {
-  const begForLife = () => console.log("Please don't leave");
-  useBeforeLeave(begForLife);
   return (
     <div className="App">
       <h1>Hi</h1>
