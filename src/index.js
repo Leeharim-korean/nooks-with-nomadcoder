12ae201453913ab01ec2d2140a4cf914018fc701
Leeharim__kr <hw05165@naver.com>
2021-07-07 16:22:43 +0900

    Delete previous content prepare new lecture

--- src/index.js
+++ src/index.js
@@ -3,33 +3,8 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useNotification = (title, options) => {
-  if (!("Notification" in window)) {
-    return;
-  }
-  const fireNotif = () => {
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
-          new Notification(title, options);
-        } else {
-          return;
-        }
-      });
-    } else {
-      new Notification(title, options);
-    }
-  };
-  return fireNotif;
-};
-
 const App = () => {
-  const tirggerNoti = useNotification("Can I steal your heart?");
-  return (
-    <div className="App">
-      <button onClick={tirggerNoti}>Hello</button>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 const rootElement = document.getElementById("root");
