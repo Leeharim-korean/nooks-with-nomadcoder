b74edf829ff19848a4ddcd8044a58ab0342a1b99
Leeharim__kr <hw05165@naver.com>
2021-07-07 16:22:20 +0900

    #2.7 useNotification build useNotification without any hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React, { useRef } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
 const App = () => {
-  return <div className="App"></div>;
+  const tirggerNoti = useNotification("Can I steal your heart?");
+  return (
+    <div className="App">
+      <button onClick={tirggerNoti}>Hello</button>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
