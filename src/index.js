83dc5ff0f762e96e6c88d2742aed9b0f9a673e9d
Leeharim__kr <hw05165@naver.com>
2021-07-07 14:15:58 +0900

    #2.5 useNetwork build useNetwork with useState and useEffect hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+export const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine || true);
+  const handleChange = () => {
+    if (onChange && typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+  return status;
+};
+
 const App = () => {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
     </div>
   );
 };
