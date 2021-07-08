54ea2706d9c684a1f04000c280b9e9aae932892c
Leeharim__kr <hw05165@naver.com>
2021-07-08 14:19:54 +0900

    #2.8 useAxios bulid useAxios with useState and useEffect hooks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import useAxios from "./useAxios";
 
 import "./styles.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const { loading, data, error, refetch } = useAxios({
+    url: "yts.am/api/v2/list_movies.json"
+  });
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+  );
 };
 
 const rootElement = document.getElementById("root");
